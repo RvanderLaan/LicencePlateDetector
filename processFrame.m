7ac1cbe72cb83f5f7dc9c0191e1f6ba76212d887
Remi van der Laan <rrm-remi@hotmail.com>
2015-01-02 19:37:43 +0100

    GUI verbetered (start/stop knop, tijdlijn, tabel) en simpele character recognition geïmplementeerd. Hiervoor was 't wel nodig om matlab R2014b te installeren en de Computer Vision System toolbox

--- processFrame.m
+++ processFrame.m
@@ -1,44 +1,65 @@
+% Process: Search yellow objects, crop the largest object out of the
+% original image and try to read characters
 function res = processFrame(frame)
 
 frameRed = frame(:, :, 1);
 frameGreen = frame(:, :, 2);
 frameBlue = frame(:, :, 3);
 
-% % Treshholding red and green
-% temp = (frameRed < (1.37*frameGreen + 55)) & (frameRed > (frameGreen + -30));
-% % Treshholding red and blue
-% temp = temp & frameRed > 0.9*frameBlue - 40;
-% % Treshholding blue and green
-% temp = temp & frameGreen > 0.95*frameBlue & frameGreen < 3.75*frameBlue;
-% 
-% % Tresholding minimum lines (no dark values)
-% temp = temp & frameRed > -2*frameGreen + 100 & frameRed < 5*frameBlue + 150;
-% % Thresholding minimum values
-% temp = temp & frameRed > 25 & frameGreen > 25 & frameBlue > 25;
-% 
-% % No blue areas
-% temp = temp & frameBlue < frameRed & frameBlue < frameGreen;
-% % No red areas
-% temp = temp & frameRed < (frameGreen + frameBlue);
-
 % Lines from graphs
-plate = frameRed < 1.33 * frameGreen + 50 & frameRed > frameGreen - 30;
-plate = plate & frameGreen > frameBlue & frameRed > frameBlue - 20;
+plate = frameRed < 1.33 * frameGreen + 50 & frameRed > frameGreen - 30 ...
+    & frameBlue < 2 * frameRed / 3 ...
+    & frameBlue < 2 * frameGreen / 3 + 10 ...
+    & frameGreen > 2 * frameRed / 3 ...
+    & (frameRed + frameGreen + frameBlue) > 60; % No dark colors
 
-% No dark colors
-plate = plate & (frameRed + frameGreen + frameBlue) > 120;
+% Close image
+plate = imclose(plate, ones(8));
 
-% Limits
-plate = plate & frameRed > 25;
-plate = plate & frameBlue < 160;
-plate = plate & frameGreen < 225;
-
-% Display segemented image in view
-% imagesc(plate);
-
-if sum(plate(:)) > 3000
-    res = 1;
-else
-    res = 0;
+% Find index of largest object in labeled image
+[L, num] = bwlabel(plate);
+maxSizeIdx = 0;
+maxSize = 0;
+for i=1:num
+    obj = (L == i);
+    size = sum(obj(:));
+    if size > maxSize
+        maxSizeIdx = i;
+        maxSize = size;
+    end;
 end;
 
+% Multiply the largest object with the original image
+plate = rgb2gray(frame) .* uint8(L == maxSizeIdx);
+
+% Get the cropping parameters
+[nonZeroRows, nonZeroColumns] = find(plate);
+topRow = min(nonZeroRows(:));
+bottomRow = max(nonZeroRows(:));
+leftColumn = min(nonZeroColumns(:));
+rightColumn = max(nonZeroColumns(:));
+
+% Extract a cropped image from the original image
+edgeSize = 5; % Remove this amount of pixels of the border
+plate = plate(topRow+edgeSize:bottomRow-edgeSize, leftColumn+edgeSize:rightColumn-edgeSize);
+
+% Create binary image by thresholding with a little lower than the mean value
+plate = plate ~= 0 & plate < mean(mean(plate)) * 0.9;
+
+% Show isolated plate
+% imshow(plate);
+
+res = '';
+% If the image isn't empty, try to read it
+if ~isempty(plate)
+    % Read characters which are present on Dutch license plates
+    result = ocr(plate, 'CharacterSet', '-0123456789BDFGHJKLNMPRSTVXYZ');
+
+    % Sort the character confidences
+    [sortedConf, sortedIndex] = sort(result.CharacterConfidences, 'descend');
+
+    % If there are at least 6 characters recognized, return them
+    if (length(sortedIndex) >= 6)
+        res = strtrim(result.Text(:)');
+    end;
+end;
