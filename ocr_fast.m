752f2e3c75c00de7da61e271d394b56261e3e92a
Remi van der Laan <rrm-remi@hotmail.com>
2015-01-07 11:36:38 +0100

    Added some characters

---
+++ ocr_fast.m
@@ -0,0 +1,9 @@
+function res = ocr_fast(characterImgs, img)
+    
+    % Label seperate objects (the characters)
+    % For every object (max 6)
+        % For every template character
+            % Find correlation
+        % The character corresponds to the highest correlation
+    %
+res = '';
